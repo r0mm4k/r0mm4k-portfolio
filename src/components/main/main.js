a8a78f2e398d88d42d0103500a2df6f2d4b1d20e
r0mmm4k <r0mmm4k@gmail.com>
2019-07-29 23:19:16 +0300

    Create Main component

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -5,7 +5,16 @@ import style from './main.module.css';
 const Main = () => {
 	return (
 		<main className={style.main}>
+			<div className={style.container}>
+				<div className={style.title}>
+					<span>Привет!</span>
+					<span>Меня зовут Рома Макаров.</span>
+					<span>Я Front-End разработчик</span>
+				</div>
+				<div className={style.photo}>
 
+				</div>
+			</div>
 		</main>
 	);
 };
