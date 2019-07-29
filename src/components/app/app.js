6251263673a59da10713efba6652684dbbd13648
r0mmm4k <r0mmm4k@gmail.com>
2019-07-29 23:37:52 +0300

    Create Skills component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import style from './app.module.css';
+
 import Header from '../header/header';
 import Main from '../main/main';
+import Skills from '../skills/skills';
 
 const App = () => {
 	return (
 		<div className={style.app}>
 			<Header/>
 			<Main/>
+			<Skills/>
 		</div>
 	);
 };
---
+++ src/components/skills/skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import style from './skills.module.css';
+
+const Skills = () => {
+	return (
+		<main className={style.skills}>
+			<div className={style.container}>
+
+			</div>
+		</main>
+	);
+};
+
+export default Skills;
