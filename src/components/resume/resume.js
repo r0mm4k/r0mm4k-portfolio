5bb43b42388e48e9d0ddb9f3bffcd78c43fd3845
Roma Makarov <r0mmm4k@gmail.com>
2019-10-08 00:21:46 +0300

    Update resume components

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -21,7 +21,7 @@ const Resume = () => {
 					<div className={style.btn}>
 						<a target='_blank' rel='noopener noreferrer' href='/assets/pdf/cv_roma_makarov.pdf'>
 							<i className='fa fa-download  fa-inverse'/>
-							Скачать PDF-версию
+							Скачать
 						</a>
 					</div>
 				</div>
@@ -30,36 +30,39 @@ const Resume = () => {
 				<div className={style.inner}>
 					<header>
 						<img src={avatar} alt='avatar'/>
-						<div className={style.title}>
-							<div className={style.primary}>
-								<h1 className={style.name}> Рома Макаров</h1>
-								<div className={style.slogan}>
-									Web-developer
+
+							<div className={style.title}>
+								<div className={style.primary}>
+									<h1 className={style.name}> Рома Макаров</h1>
+									<div className={style.slogan}>
+										Junior Frontend Developer
+									</div>
+									<div>
+										<a style={{marginBottom: '.8rem'}} href='mailto:r0mmm4k@gmail.com'><i
+											style={{paddingLeft: '.02rem'}}
+											className='fa fa-envelope'/>r0mmm4k@gmail.com</a>
+										<a href='tel:+375292544449'><i className='fa fa-phone'/>+375 (29) 254-44-49</a>
+									</div>
 								</div>
-								<div>
-									<a href='mailto:r0mmm4k@gmail.com'><i style={{paddingLeft: '.02rem'}} className='fa fa-envelope'/>r0mmm4k@gmail.com</a>
+								<div className={style.secondary}>
+									{socialsElements}
 								</div>
-								<div>
-									<a href='tel:+375292544449'><i className='fa fa-phone'/>+375 (29) 254-44-49</a>
-								</div>
-							</div>
-							<div className={style.secondary}>
-								{socialsElements}
-							</div>
+
 						</div>
 					</header>
 					<div className={style.body}>
 						<section>
 							<h2>
-								ОПЫТ РАБОТЫ
+								Обо мне
 							</h2>
 							<div>
-								Я начинающий web-developer, специализирующийся на front-end, back-end и разработке одностраничных
-								приложений (SPA). Коммерческого опыта по специальности нет. Опыт получаю на своих проектах. Готов упорно
-								работать и обучаться для достижения своих целей!
+								Я начинающий веб разработчик. Владею актуальным набором технологий в области Front-End, параллельно
+								изучаю Back-End. Готов начать свой трудовой путь в сфере веб разработки, где смогу получить и
+								использовать больше знаний и практического опыта. Имею большой интерес и желание достичь
+								профессионального уровня.
 							</div>
 						</section>
-						<div className={style.row}>
+						{/*<div className={style.row}>
 							<div className={style.left}>
 								<section>
 									<h2>
@@ -112,7 +115,7 @@ const Resume = () => {
 									</div>
 								</section>
 							</div>
-						</div>
+						</div>*/}
 					</div>
 				</div>
 			</article>
